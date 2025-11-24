68d1e9214631473fa02ddfeec516a1f2c18dced4
Yashasvi <yashpundir7@gmail.com>
2025-11-24 20:51:05 +0000

    conflicting paths fix

--- v2xumllm/inference.py
+++ v2xumllm/inference.py
@@ -68,7 +68,7 @@ def parse_args():
     parser.add_argument("--model_base", type=str, default="lmsys/vicuna-7b-v1.5")
     parser.add_argument("--pretrain_mm_mlp_adapter", type=str, default="checkpoints/llava-vicuna-v1-5-7b-stage1/mm_projector.bin")
     parser.add_argument("--stage2", type=str, default="checkpoints/v2xumllm-vicuna-v1-5-7b-stage2-e2")
-    parser.add_argument("--video_path", type=str, default="demo/jump.mp4")
+    parser.add_argument("--video_path", type=str, default="demo/wait.mp4")
     args = parser.parse_args()
 
     return args
